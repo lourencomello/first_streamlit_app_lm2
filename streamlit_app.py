0b3c36e84528cbdfeacc610eaff7dd6af94b0d5b
lourencomello <140459234+lourencomello@users.noreply.github.com>
2023-07-31 11:30:23 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-streamlit.title ('My Parents New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
-streamlit.text ('🥣 Omega 3 & Bluberry Oatmeal')
-streamlit.text ('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text ('🐔 Hard-Boiled Free Range Egg')
-streamlit.text ('🥑🍞 Avocado Toast')
+streamlit.text('🥣 Omega 3 & Bluberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
